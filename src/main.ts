e5f4511f87c44808a090d2bdf22509d80aa5ea84
mlane <mlane@travelpassgroup.com>
2025-12-21 07:20:46 +0100

    initial-electron implementation

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,56 @@
 import './monaco-env'
 import * as monaco from 'monaco-editor'
 import './style.css'
-import { loadConfigs } from './utils'
+import { inferLanguage, loadConfigs } from './utils'
 
 const root = document.getElementById('root')
+const search = document.getElementById('search')
 
 if (!root) throw new Error('Root element not found')
 
-const innerModel = monaco.editor.createModel(
-  `class Test {\n\tconstructor() {}\n}`,
-  'typescript',
-  monaco.Uri.parse('file:///main.ts')
-)
+if (!search) throw new Error('Search element not found')
 
 const config = await loadConfigs()
 const { language, model, uri, value, ...userOptions } = config ?? {}
 
-if (language || model || uri || value)
+if (language || model || uri || value) {
   console.warn(
     'embertext configuration cannot contain model, language, uri, or value'
   )
+}
 
-monaco.editor.create(root, {
-  model: innerModel,
+monaco.typescript.typescriptDefaults.setCompilerOptions({
+  jsx: monaco.typescript.JsxEmit.ReactJSX,
+  target: monaco.typescript.ScriptTarget.ESNext,
+  module: monaco.typescript.ModuleKind.ESNext,
+})
+
+monaco.typescript.typescriptDefaults.setDiagnosticsOptions({
+  noSemanticValidation: true,
+})
+
+const editor = monaco.editor.create(root, {
   ...userOptions,
+})
+
+/** @todo replace with input event and use keydown for file selection */
+search.addEventListener('keydown', async event => {
+  try {
+    if (event.key !== 'Enter') return
+
+    const path = 'public/test.js'
+    const content = await window.embertext.readFile(path)
+    const uri = monaco.Uri.parse(`file:///${path}`)
+    const language = inferLanguage(path)
+
+    let model = monaco.editor.getModel(uri)
+
+    if (!model) {
+      model = monaco.editor.createModel(content, language, uri)
+    }
+
+    editor.setModel(model)
+  } catch (error) {
+    console.error(error)
+  }
 })
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,23 @@
+export const inferLanguage = (path: string): string => {
+  const language = path?.split('.')?.pop() ?? 'plaintext'
+
+  if (language === 'css') return 'css'
+
+  if (language === 'html') return 'html'
+
+  if (language === 'js') return 'javascript'
+
+  if (language === 'json') return 'json'
+
+  if (language === 'jsx') return 'javascriptreact'
+
+  if (language === 'ts') return 'typescript'
+
+  if (language === 'tsx') return 'typescript'
+
+  return 'plaintext'
+}
+
 export const loadConfigs = async () => {
   try {
     const response = await fetch('/.embertext.json')
